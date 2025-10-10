dbda3a10e94780517b934d7699aeaa55e527d8ff
Dhiraj Malla <malladhiraj@gmail.com>
2025-10-10 05:05:49 +0000

    test

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.2.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Fetch the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "tf-demo-aws-ec2-instance-1" {
+  ami           = "ami-0005e0cfe09cc9050"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "tf-demo-aws-ec2-instance-1"
+  }
+
+}
