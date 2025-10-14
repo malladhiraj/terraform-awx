0e4a3f428728e6be2c07b02301fb7af300d82ede
Dhiraj Malla <malladhiraj@gmail.com>
2025-10-13 23:41:27 -0400

    update to awx to app

--- terraform-with-aap-provider/main.tf
+++ terraform-with-aap-provider/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
   }
 }
 
-provider "app" {
+provider "aap" {
   host     = "http://192.168.2.218:10445"
   username = "kali"
   password = "kali"
